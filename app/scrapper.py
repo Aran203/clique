557040c923cd5e1400910bb105e8794c9a24eeeb
Karan Bhalla <karanbhalla204@gmail.com>
2025-03-30 12:46:21 -0500

    Add feature to process labs

--- app/scrapper.py
+++ app/scrapper.py
@@ -11,8 +11,8 @@ def scrape_kattis_submissions(url):
         print(f"Request failed: {e}")
         return None
     
-def process_solve_period(soup: BeautifulSoup, filename):
-    '''returns a dict mapping each student to (number of problems solved, which problems they solved)'''
+def process_problem_set(soup: BeautifulSoup, filename):
+    '''returns a dict mapping each student to (number of problems solved, which problems they solved) for a problem set'''
 
     student_info = {}
 
@@ -51,6 +51,45 @@ def process_solve_period(soup: BeautifulSoup, filename):
     
     return student_info
     
+def process_lab(soup: BeautifulSoup, filename):
+    '''returns a dict mapping each student to (number of problems solved, which problems they solved) for lab'''
+
+    student_info = {}
+
+    with open(filename) as f:
+        for row in f.readlines():
+            student_info[row.strip()] = None 
+
+
+    table = soup.find("table")
+    rows = table.find_all("tr")
+
+
+    for row in rows:
+        counter = 0
+        name = num_solved = None
+        problems_solved = []
+
+        for td in row.find_all("td"):
+            
+            # col 1 is name; col 2 is num solved, col 4 -> end are problems
+            if counter == 1:
+                name = td.text.strip()
+            elif counter == 2:
+                num_solved = int(td.text.strip())
+            elif counter >= 4:
+                status = td.get("class", None)
+
+                if status and status[0].strip() == "solved":
+                    problems_solved.append(chr(counter + 61))
+            counter += 1
+    
+
+        if name and name in student_info:
+            student_info[name] = (num_solved, problems_solved)
+
+    
+    return student_info
     
 
 
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from app import *
 import pandas as pd
 
-# root url to scrap; change to fit semester in which course is taught
+# root url to scrape; change to fit semester in which course is taught
 BASE_URL = "https://tamu.kattis.com/courses/CSCE430/2025Spring/assignments"
 
 
-def fetch_results(code, filename):
+def fetch_results(code, filename, is_problem_set, solve):
     URL = f'{BASE_URL}/{code}/standings'
 
     soup = scrape_kattis_submissions(URL)
@@ -13,27 +13,49 @@ def fetch_results(code, filename):
     if not soup:
         raise ValueError(f"HTML content could not be obtained.\n\nCode provided is {code}\nURL scraped is {URL}")
     
-    results = process_solve_period(soup, filename)
+    if (is_problem_set):
+        results = process_problem_set(soup, filename)
+    else:
+        if (solve):
+            results = process_lab(soup, filename)
+        else:
+            results = process_problem_set(soup, filename)
 
     return results
 
 
 def main():
+    print("Is assignment being processed a problem set? (yes/no) ", end = "")
+    is_problem_set = input().lower() 
+
+    if ("yes" in is_problem_set or "y" in is_problem_set):
+        is_problem_set = True
+    else:
+        is_problem_set = False
+
     print("Enter assignment code: ", end = "")
     code = input()
 
     print("Enter file containing student names to process: ", end = "")
     filename = input()
 
-    print("Is assignment being processed for the solve period? (yes/no) ", end = "")
-    is_upsolve = input()
+    print("Is assignment being processed for SOLVE period? (yes/no) ", end = "")
+    is_solve = input().lower()
 
-    if ("yes" not in is_upsolve.lower()):
-        print("Enter code for corresponding solve period: ", end = "")
+    if ("yes" in is_solve or "y" in is_solve):
+        is_solve = True
+    else:
+        is_solve = False
+
+
+
+    # Processing Upsolve Period
+    if (not is_solve):
+        print("Enter code for corresponding SOLVE period: ", end = "")
         solve_code = input()
 
-        upsolve_raw = fetch_results(code, filename)
-        solve_results = fetch_results(solve_code, filename)
+        upsolve_raw = fetch_results(code, filename, is_problem_set, False)
+        solve_results = fetch_results(solve_code, filename, is_problem_set, True)
         results = {}
 
         for student in solve_results:
@@ -44,9 +66,10 @@ def main():
             upsolved = all - solved
             results[student] = (len(upsolved), list(upsolved))
     else:
-        results = fetch_results(code, filename)
+        results = fetch_results(code, filename, is_problem_set, is_solve)
 
 
+    # convert results to csv
     data = pd.DataFrame(parse_results(results))
     data.to_csv(f'{code}_results.csv', index = False)
 
